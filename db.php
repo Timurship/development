39daa875302f39882bab80f79fa3ccff158ecdfd
gainullint <gainullint@localhost.localdomain>
2021-12-21 07:28:09 +0300

    master

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","Timur1805","register");
+$con = mysqli_connect("mysql","root","Str0ngPaSSw0rd","register");
 if (mysqli_connect_errno())
 {
 echo "Failed to connect to MySQL: " . mysqli_connect_error();
